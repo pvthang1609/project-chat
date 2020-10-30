eecdbac8724cc7b4ead54bb63e137768ae87e43e
pvthang1609 <pvthang1609@gmail.com>
2020-10-30 17:26:54 +0700

    add loading, query  data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useAuthState } from "react-firebase-hooks/auth";
+import {useCollectionData} from 'react-firebase-hooks/firestore';
 
 import firebase from "./components/Firebase/configFirebase";
 import SignIn from "./components/SignIn";
 import ChatRoom from "./components/ChatRoom";
 import SignOut from "./components/SignOut";
+import Loading from "./components/Loading";
 
 const auth = firebase.auth();
 const fireStore = firebase.firestore();
@@ -21,15 +23,26 @@ function App() {
   const [user] = useAuthState(auth);
   //include user, loading, error
   //user include displayName, email, photoURL, emailVerified, uid
+  const messagesRef = fireStore.collection('messages') // chỉ là tham chiếu của collection messages
+  const query = messagesRef.orderBy('timeInit').limit(25)
+  const [ value, loading, error ] = useCollectionData(query , {
+    idField: "id",
+  })
 
+  
   return (
     <div>
+      {console.log(loading ? 'loading' : 'isNot')}
       {user ? (
-        <ChatRoom logout={logout} userName={user.displayName} />
+        <ChatRoom userName={user.displayName} values={value} />
       ) : (
         <SignIn login={login} />
       )}
       {auth.currentUser && <SignOut logout={logout} />}
+      {value ? value.map((message, index) => {
+        return <p key={index}>{message.content}</p>
+      }) : <Loading/>}
+      <Loading/>
     </div>
   );
 }
---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './loading.css';
+
+export default function Loading() {
+    return (
+        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
